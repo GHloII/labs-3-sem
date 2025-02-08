c141c9ca9bd2b4002c99d138e63e7f4081b844db
Hlo_mac <glebhlebt@gmail.com>
2025-02-08 17:53:28 +0300

    first commit

---
+++ Development of software systems/lab-1/lab-1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+
+    {
+     
+        static void Main(string[] args)
+        {
+            // show greets;
+            int command;
+            while (true)
+            {
+                Console.WriteLine("1 - запустить программу" +
+                                "\n2 - выйти из программы  ");
+                command = InputNumFromSTDIN();
+
+                switch (command)
+                {
+                    case 1:
+                        Console.WriteLine("запуск выполнен");
+                        //menu();
+                        break;
+                    case 2:
+                        Console.WriteLine("выход выполнен");
+                        return;
+                    default:
+                        Console.WriteLine("команда не найдена");;
+                        break;
+                }
+            }
+        }
+
+
+        static int InputNumFromSTDIN()
+        {
+
+            while (true) {
+                string? str_num = Console.ReadLine();
+
+                if (str_num != null && int.TryParse(str_num, out int num))
+                {
+                    return num;
+                }
+
+                Console.WriteLine("ошибка");
+            }
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("Выберите способ заполнения:");
+
+        }
+
+        
+
+    }
+}
