963a87e9ccf5f834557aeb5b9d5058ed3e3b2995
GlebPC <glebhlebt@gmail.com>
2025-05-24 20:48:45 +0300

    add: tests for geometry

--- Development of software systems/lab-1/TestProject/UnitTest1.cs
+++ Development of software systems/lab-1/TestProject/UnitTest1.cs
@@ -41,7 +41,7 @@ namespace lab_1.Tests
             var result = WindowsNameValidator.IsInvalidWindowsName("CON.txt", out var reason);
 
             Assert.IsTrue(result);
-            Assert.AreEqual("Имя \"COM1\" является зарезервированным в Windows.", reason);
+            Assert.AreEqual("Имя \"CON\" является зарезервированным в Windows.", reason);
         }
 
         [TestMethod]
@@ -53,4 +53,133 @@ namespace lab_1.Tests
             Assert.AreEqual("", reason);
         }
     }
+
+
+
+    [TestClass]
+   
+    public class MathGeometrySolverTests
+    {
+        [TestMethod]
+        public void Distance_TwoPoints_ReturnsCorrectDistance()
+        {
+            var p1 = new Point(0, 0);
+            var p2 = new Point(3, 4);
+            var dist = MathGeometrySolver.Distance(p1, p2);
+            Assert.IsTrue(MathGeometrySolver.IsEqual(dist, 5));
+        }
+
+        [TestMethod]
+        public void IsEqual_CloseValues_ReturnsTrue()
+        {
+            Assert.IsTrue(MathGeometrySolver.IsEqual(1.0000001, 1.0000002));
+        }
+
+        [TestMethod]
+        public void IsEqual_DifferentValues_ReturnsFalse()
+        {
+            Assert.IsFalse(MathGeometrySolver.IsEqual(1.0001, 1.002));
+        }
+
+        [TestMethod]
+        public void IsAngle90_RightAngle_ReturnsTrue()
+        {
+            var p1 = new Point(0, 0);
+            var p2 = new Point(0, 1);
+            var p3 = new Point(1, 1);
+            Assert.IsTrue(MathGeometrySolver.IsAngle90(p1, p2, p3));
+        }
+
+        [TestMethod]
+        public void IsAngle90_NotRightAngle_ReturnsFalse()
+        {
+            var p1 = new Point(0, 0);
+            var p2 = new Point(1, 1);
+            var p3 = new Point(2, 2);
+            Assert.IsFalse(MathGeometrySolver.IsAngle90(p1, p2, p3));
+        }
+
+        [TestMethod]
+        public void HasOverlap_ColinearAndOverlapping_ReturnsTrue()
+        {
+            var s1 = new Segment(new Point(0, 0), new Point(2, 0));
+            var s2 = new Segment(new Point(1, 0), new Point(3, 0));
+            Assert.IsTrue(MathGeometrySolver.HasOverlap(s1, s2));
+        }
+
+        [TestMethod]
+        public void HasOverlap_ColinearButNotOverlapping_ReturnsFalse()
+        {
+            var s1 = new Segment(new Point(0, 0), new Point(1, 0));
+            var s2 = new Segment(new Point(2, 0), new Point(3, 0));
+            Assert.IsFalse(MathGeometrySolver.HasOverlap(s1, s2));
+        }
+
+        [TestMethod]
+        public void HasOverlap_NotColinear_ReturnsFalse()
+        {
+            var s1 = new Segment(new Point(0, 0), new Point(1, 1));
+            var s2 = new Segment(new Point(0, 1), new Point(1, 2));
+            Assert.IsFalse(MathGeometrySolver.HasOverlap(s1, s2));
+        }
+
+        [TestMethod]
+        public void FindIntersection_IntersectingSegments_ReturnsIntersectionPoint()
+        {
+            var s1 = new Segment(new Point(0, 0), new Point(2, 2));
+            var s2 = new Segment(new Point(0, 2), new Point(2, 0));
+            bool isOverlap = false;
+
+            var intersection = MathGeometrySolver.FindIntersection(s1, s2, ref isOverlap);
+
+            Assert.IsNotNull(intersection);
+            Assert.IsTrue(MathGeometrySolver.IsEqual(intersection.x, 1));
+            Assert.IsTrue(MathGeometrySolver.IsEqual(intersection.y, 1));
+            Assert.IsFalse(isOverlap);
+        }
+
+        [TestMethod]
+        public void FindIntersection_OverlappingSegments_ReturnsNullAndOverlapTrue()
+        {
+            var s1 = new Segment(new Point(0, 0), new Point(2, 0));
+            var s2 = new Segment(new Point(1, 0), new Point(3, 0));
+            bool isOverlap = false;
+
+            var result = MathGeometrySolver.FindIntersection(s1, s2, ref isOverlap);
+
+            Assert.IsNull(result);
+            Assert.IsTrue(isOverlap);
+        }
+
+        [TestMethod]
+        public void FindIntersection_ParallelNonOverlapping_ReturnsNull()
+        {
+            var s1 = new Segment(new Point(0, 0), new Point(1, 0));
+            var s2 = new Segment(new Point(0, 1), new Point(1, 1));
+            bool isOverlap = false;
+
+            var result = MathGeometrySolver.FindIntersection(s1, s2, ref isOverlap);
+
+            Assert.IsNull(result);
+            Assert.IsFalse(isOverlap);
+        }
+
+        [TestMethod]
+        public void FindIntersection_NotIntersecting_ReturnsNull()
+        {
+            var s1 = new Segment(new Point(0, 0), new Point(1, 1));
+            var s2 = new Segment(new Point(2, 2), new Point(3, 3));
+            bool isOverlap = false;
+
+            var result = MathGeometrySolver.FindIntersection(s1, s2, ref isOverlap);
+
+            Assert.IsNull(result);
+            Assert.IsFalse(isOverlap);
+        }
+    }
+
+
+
+
+
 }
